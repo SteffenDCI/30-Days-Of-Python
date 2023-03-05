3c4c26454c6950c25013de6e7b6ef28117daf2b0
Steffen P <steffen.pretzel@gmx.de>
2023-03-05 18:07:13 +0100

    Day 1 solved

---
+++ 01_Day_Introduction/01_first_program.md
@@ -0,0 +1,51 @@
+# Introduction
+
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3) # addition(+)
+
+print(3 - 1) # subtraction(-)
+
+print(2 \* 3) # multiplication(\_)
+
+print(3 / 2) # division(/)
+
+print(3 ** 2) # exponential(**)
+
+print(3 % 2) # modulus(%)
+
+print(3 // 2) # Floor division operator(//)
+
+# Checking data types
+
+### Int - Integer
+
+print(type(10))
+
+### Float (Floating point number)
+
+print(type(3.14))
+
+### Complex (Complex number)
+
+print(type(1 + 3j))
+
+### String
+
+print(type('My_name'))
+
+### List
+
+print(type([1, 2, 3]))
+
+### Dictionary
+
+print(type({'name':'My_name'}))
+
+### Set
+
+print(type({9.8, 3.14, 2.7}))
+
+### Tuple
+
+print(type((9.8, 3.14, 2.7)))
--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,21 +1,22 @@
-# Introduction
-# Day 1 - 30DaysOfPython Challenge
+# name = input("Please enter your name: ")
+# print("Hello World, my name is", name, "!")
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
-print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+
+print(2 + 3)    # addition
+print(3 - 1)    # subtraction
+print(2 * 3)    # multiplication
+print(3 / 2)    # division, result is a float
+print(3 ** 2)   # exponential
+print(3 % 2)    # modulus(%), calculates the remainder after division
+print(7 // 2)   # floor division operator (//)
+
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(10))                 # integer
+print(type(3.14))               # float
+print(type(1 + 3j))             # complex number
+print(type('name'))             # string
+print(type([1, 2, 3]))          # list
+print(type({9.8, 'string', 3}))  # set
+print(type((9.8, 3.14, 2.7)))   # tuple
